ad9b8df701064383f10b00a273b3441e0f76d0fb
Dani <daniprofe@gmail.com>
2019-03-27 21:32:10 +0100

    Babel and prettify some things...

---
+++ demos/01/testing_babel.js
@@ -0,0 +1,9 @@
+let worker = {
+    name: 'Peter',
+    age: 22,
+    country: 'Spain'
+};
+
+let {name, age, country} = worker;
+
+console.log(`${name} is a ${age} years old an lives in ${country}`);
